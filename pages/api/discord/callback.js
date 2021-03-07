6d45d94c9026a134d1e6e7929a9def4660e5fd80
UniqueDevStorm <storm@uniquecode.team>
2021-03-07 15:23:53 +0900

    feat: callback

--- pages/api/discord/callback.js
+++ pages/api/discord/callback.js
@@ -1,3 +1,20 @@
-export default (req, res) => {
-    res.send(req.query.code)
+import * as process from "process";
+
+export default async (req, res) => {
+    const DISCORD_API_ENDPOINT = 'https://discord.com/api/v8'
+    const code = req.query.code;
+    const params = new URLSearchParams({
+        client_id: process.env.CLIENT_ID,
+        code,
+        client_secret: process.env.CLIENT_SECRET,
+        redirect_uri: 'http://localhost:3000/',
+        grant_type: 'authorization_code',
+        scope: 'identify email guilds'
+    })
+    const request = (await (await fetch(`${DISCORD_API_ENDPOINT}/oauth2/token`, {
+        method: 'POST',
+        body: params,
+        headers: { 'Content-Type' : 'application/json' }
+    })).json())
+    res.send(request)
 }
