140b4d7181aed89c000e2b55cbda21b1af91e3f9
UniqueDevStorm <storm@uniquecode.team>
2021-03-07 15:37:35 +0900

    feat: get access_token

--- pages/api/discord/callback.js
+++ pages/api/discord/callback.js
@@ -2,19 +2,20 @@ import * as process from "process";
 
 export default async (req, res) => {
     const DISCORD_API_ENDPOINT = 'https://discord.com/api/v8'
+    const REDIRECT_URI = 'http://localhost:3000/api/discord/callback'
     const code = req.query.code;
     const params = new URLSearchParams({
         client_id: process.env.CLIENT_ID,
         code,
         client_secret: process.env.CLIENT_SECRET,
-        redirect_uri: 'http://localhost:3000/',
+        redirect_uri: REDIRECT_URI,
         grant_type: 'authorization_code',
         scope: 'identify email guilds'
     })
-    const request = (await (await fetch(`${DISCORD_API_ENDPOINT}/oauth2/token`, {
+    const token = (await (await fetch(`${DISCORD_API_ENDPOINT}/oauth2/token`, {
         method: 'POST',
         body: params,
-        headers: { 'Content-Type' : 'application/json' }
-    })).json())
-    res.send(request)
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+    })).json()).access_token;
+    res.send(token)
 }
